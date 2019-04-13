5eaaaee394c099d0048fdb1bcee47352a1700989
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-13 21:56:14 +0430

    branch sepehr built

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("hello");
-        System.out.println("holy grail");
+        System.out.println("branch sepehr built");
     }
 }
