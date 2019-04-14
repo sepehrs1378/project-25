1ebc707b0ca3b836bb7438854757f0d0072cc111
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-14 12:16:30 +0430

    class Player added

---
+++ src/Player.java
@@ -0,0 +1,38 @@
+class Player {
+    private PlayerInfo playerInfo;
+    private Hand hand;
+    private Deck deck;
+    private int mana;
+
+    public PlayerInfo getPlayerInfo() {
+        return playerInfo;
+    }
+
+    public Hand getHand(){
+        return hand;
+    }
+
+    public Deck getDeck(){
+        return deck;
+    }
+
+    public int getMana(){
+        return mana;
+    }
+
+    public void setPlayerInfo(PlayerInfo playerInfo){
+        this.playerInfo=playerInfo;
+    }
+
+    public void setHand(Hand hand){
+        this.hand=hand;
+    }
+
+    public void setDeck(Deck deck){
+        this.deck=deck;
+    }
+
+    public void setMana(int mana){
+        this.mana=mana;
+    }
+}
