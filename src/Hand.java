417d6cb32d23d3f5fde1483d8574342599301df2
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-14 12:40:06 +0430

    class Hand added

---
+++ src/Hand.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Hand {
+    private List<Card> cards=new ArrayList<Card>();
+
+    public List<Card> getCards(){
+        return cards;
+    }
+
+    public void addCard(Card newCard){
+
+    }
+
+    public void deleteCard(Card card){
+
+    }
+
+    public Card getNextCard(){
+
+    }
+}
