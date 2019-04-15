6532e17ead54b84ff5fedf7dd4e23b84d52a28a3
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 11:16:16 +0430

    class PlayerCollection added

---
+++ src/PlayerCollection.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class PlayerCollection {
+    private List<Card> cards=new ArrayList<Card>();
+    private List<Item> items=new ArrayList<Item>();
+
+    public List<Card> getCards(){
+        return cards;
+    }
+
+    public List<Item> getItems(){
+        return items;
+    }
+
+    public void addCard(Card newCard){
+
+    }
+
+    public void addItem(Item newItem){
+
+    }
+
+    public void deleteCard(Card card){
+
+    }
+
+    public void deleteItem(Item item){
+
+    }
+}
