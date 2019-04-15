c36d4279b06d81ee1b390e62b7700f251af3d663
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 15:03:01 +0430

    added class BattleGround

---
+++ src/BattleGround.java
@@ -0,0 +1,27 @@
+class BattleGround {
+    private Cell[][] cells=new Cell[5][9];
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public int getWidthOfUnit(Unit unit){
+
+    }
+
+    public int getLengthOfUnit(Unit unit){
+
+    }
+
+    public boolean doesHaveUnit(Unit unit){
+
+    }
+
+    public int getNumberOfFlags(){
+
+    }
+
+    public int getNumberOfFlagsOnGround(){
+
+    }
+}
