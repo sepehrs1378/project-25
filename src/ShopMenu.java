680925f484bc16b07049fe49e4aeefbcf6d68fa4
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 11:30:17 +0430

    class ShopMenu added

---
+++ src/ShopMenu.java
@@ -0,0 +1,29 @@
+class ShopMenu {
+    private ShopMenu(){
+
+    }
+
+    public static void exit(){
+
+    }
+
+    public static void showCollection(){
+
+    }
+
+    public static void searchItemOrCard(String name){
+
+    }
+
+    public static void buy(String name){
+
+    }
+
+    public static void sell(String name){
+
+    }
+
+    public static void show(){
+
+    }
+}
