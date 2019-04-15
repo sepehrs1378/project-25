6cd75a9c711e221b38679c4145478ae6e9116db5
AS2C <ali.saraertoosi@yahoo>
2019-04-15 21:33:31 +0430

    MatchInfo

--- src/MatchInfo.java
+++ src/MatchInfo.java
@@ -1,33 +1,51 @@
-import java.util.Date;
+import java.time.LocalDateTime;
 
-class MatchInfo {
-    public static final String OPPONENT = "opponent";
-    public static final String PLAYER_HIM_SELF = "player him self";
-    private Account opponet;
-    private String winner;
-    private Date matchDate;
+public class MatchInfo extends Account {
+    private Account opponent;
+    private Account winner;
+    private LocalDateTime matchDate;
 
-    public Account getOpponet() {
-        return opponet;
+    public void setOpponent(Account opponent) {
+        this.opponent = opponent;
     }
 
-    public String getWinner() {
-        return winner;
-    }
-
-    public Date passedTime() {
-
-    }
-
-    public void setOpponet(Account opponet) {
-        this.opponet = opponet;
-    }
-
-    public void setWinner(String winner) {
+    public void setWinner(Account winner){
         this.winner = winner;
     }
 
-    public void setMatchDate(Date matchDate) {
-        this.matchDate = matchDate;
+    public void setMatchDate(){
+        this.matchDate = LocalDateTime.now();
     }
+
+    public Account getOpponent(){
+        return opponent;
+    }
+
+    public Account getWinner(){
+        return winner;
+    }
+
+    public LocalDateTime getMatchDate(){
+        return matchDate;
+    }
+
+    public LocalDateTime getPassedTime(){
+
+    }
+
+    private void calculatePassedTime(LocalDateTime matchDate){    //todo this still has a lot of problems
+        String[] dateAndTimeOfMatch = matchDate.toString().split("T");
+        String[] timeOfMatch = dateAndTimeOfMatch[1].split(":");
+        String[] dateAndTimeRightNow = LocalDateTime.now().toString().split("T");
+        String[] timeRightNow = dateAndTimeRightNow[1].split(":");
+        int hourOfMatch = Integer.parseInt(timeOfMatch[0]);
+        int hourRightNow = Integer.parseInt(timeRightNow[0]);
+        int howManyHoursPassed = hourRightNow - hourOfMatch;
+        int minuteOfMatch = Integer.parseInt(timeOfMatch[1]);
+        timeOfMatch[2] = timeOfMatch[2].replaceAll("(\\d+)[.](\\d+)", "$1");
+        int secondOfMatch = Integer.parseInt(timeOfMatch[2]);
+
+
+    }
+
 }
