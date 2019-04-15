842710b9dc7179fd01dbe45d2292c08dfa073bc1
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 11:43:17 +0430

    added class Usable

---
+++ src/Usable.java
@@ -0,0 +1,16 @@
+public class Usable {
+    private int price;
+    private Spell passivePower;
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price=price;
+    }
+
+    public void doSpell(){
+
+    }
+}
