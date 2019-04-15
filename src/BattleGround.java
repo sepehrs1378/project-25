ed83d511e18e7110da6fbb0fc00c708a897ef6af
parhamsaremi <pa.sa.13792000@outlook.com>
2019-04-15 13:10:42 +0430

    adding Battle and BattleGround and Cell classes

---
+++ src/BattleGround.java
@@ -0,0 +1,22 @@
+public class BattleGround {
+    private Cell[][] cells=new Cell[5][9];
+    public Cell[][] getCells(){
+        return this.cells;
+    }
+    public Cell getCoordinatesOfUnit(Unit unit){
+        for(Cell[] cellRow:cells)
+        for (Cell cell:cellRow){
+            if (cell.getCrad(Unit)==Unit){
+                return cell;
+            }
+        }
+        return null;
+    }
+    public boolean doesHaveUnit(Unit unit){
+        if(getCoordinatesOfUnit(unit)==null){
+            return false;
+        }
+        return true;
+    }
+
+}
