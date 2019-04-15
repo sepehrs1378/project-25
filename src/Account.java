19fec7d168809bd654f4e19fc025c26fa29a25a4
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 20:29:09 +0430

    added class Account

---
+++ src/Account.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Account {
+    private static List<Account> accounts=new ArrayList<>();
+    private boolean[] levelsOpennessStatus =new boolean[3];
+    private String password;
+    private String username;
+    private PlayerInfo playerInfo;
+    private List<MatchInfo> matchList=new ArrayList<>();
+
+    {
+        levelsOpennessStatus[0]=true;
+    }
+
+    public static List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public static void addAccount(Account newAccount){
+
+    }
+
+    public static boolean checkValidation(String username,String password){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public PlayerInfo getPlayerInfo() {
+        return playerInfo;
+    }
+
+    public boolean[] getLevelsOpennessStatus() {
+        return levelsOpennessStatus;
+    }
+
+    public List<MatchInfo> getMatchList() {
+        return matchList;
+    }
+
+    public void addMatchToMatchList(MatchInfo Match){
+
+    }
+
+    public void openLevelOfStory(int level){
+        levelsOpennessStatus[level]=true;
+    }
+}
