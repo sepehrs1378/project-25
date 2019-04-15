dcde3802dd99ff7d12a6945930b6ec5657de4604
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-15 11:33:01 +0430

    added class Item

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+class Item {
+    private String itemID;
+
+    public String getItemID(){
+        return itemID;
+    }
+
+    public void setItemID(String itemID){
+        this.itemID=itemID;
+    }
+}
