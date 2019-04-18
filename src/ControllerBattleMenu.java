85251e10c6568224304eab19360ca332f1fbc703
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-18 23:47:24 +0430

    added classes ControllerBattleMenu and ControllerMainMenu

---
+++ src/ControllerBattleMenu.java
@@ -0,0 +1,14 @@
+public class ControllerBattleMenu {
+    private static ControllerBattleMenu ourInstance = new ControllerBattleMenu();
+
+    public static ControllerBattleMenu getInstance() {
+        return ourInstance;
+    }
+
+    private ControllerBattleMenu() {
+    }
+
+    public void main(){
+
+    }
+}
--- src/ControllerMainMenu.java
+++ src/ControllerMainMenu.java
@@ -1,5 +1,6 @@
 public class ControllerMainMenu {
     private static ControllerMainMenu ourInstance = new ControllerMainMenu();
+    private static final View view = View.getInstance();
 
     public static ControllerMainMenu getInstance() {
         return ourInstance;
@@ -8,7 +9,44 @@ public class ControllerMainMenu {
     private ControllerMainMenu() {
     }
 
-    public void main(){
+    public void main() {
+        boolean didLogout = false;
+        Request request = new Request();
+        request.getNewCommand();
+        while (!didLogout) {
+            switch (request.getType()) {
+                case ENTER:
+                    break;
+                case SAVE:
+                    break;
+                case LOGOUT:
+                    didLogout = true;
+                    break;
+                default:
+                    System.out.println("!!!!!! bad input in ControllerMainMenu.main");
+                    System.exit(-1);
+            }
+        }
+    }
 
+    public void save() {
+
+    }
+
+    public void enter(Request request) {
+        switch (request.getCommand()) {
+            case "enter collection":
+                ControllerCollection.getInstance().main();
+                break;
+            case "enter shop":
+                ControllerShop.getOurInstance().main();
+                break;
+            case "enter battle":
+                ControllerBattleMenu.getInstance().main();
+                break;
+            default:
+                request.setErrorType(ErrorType.WRONG_COMMAND);
+                view.printError(request.getErrorType());
+        }
     }
 }
