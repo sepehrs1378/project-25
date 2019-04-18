972517204ee93c81c00579b214741044a348fd36
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-18 22:56:24 +0430

    working on yet...

--- src/Spell.java
+++ src/Spell.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Spell extends Card {
-    public static final String PASSIVE="passive";
-    public static final String CASTABLE="castable";
+    public static final String PASSIVE = "passive";
+    public static final String CASTABLE = "castable";
     private Target target;
     private int cooldown;
     private int apChange;
@@ -9,60 +12,65 @@ class Spell extends Card {
     private List<Buff> deletedBuffs = new ArrayList<Buff>();
     private List<Unit> addedUnits = new ArrayList<Unit>();
     private String passiveOrCastable;
+    private String description;
 
-    public Target getTarget(){
+    public Target getTarget() {
         return target;
     }
 
-    public int getCooldown(){
+    public int getCooldown() {
         return cooldown;
     }
 
-    public int getApChange(){
+    public int getApChange() {
         return apChange;
     }
 
-    public int getHpChange(){
+    public int getHpChange() {
         return hpChange;
     }
 
-    public void setTarget(Target target){
-        this.target=target;
+    public String getDescription() {
+        return description;
     }
 
-    public void setCooldown(int cooldown){
-        this.cooldown=cooldown;
+    public void setTarget(Target target) {
+        this.target = target;
     }
 
-    public void setApChange(int apChange){
-        this.apChange=apChange;
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
     }
 
-    public void setHpChange(int hpChange){
-        this.hpChange=hpChange;
+    public void setApChange(int apChange) {
+        this.apChange = apChange;
     }
 
-    public void doSpell(){
-
+    public void setHpChange(int hpChange) {
+        this.hpChange = hpChange;
     }
 
-    public void changeAp(){
-
-    }
-
-    public void changeHp(){
+    public void doSpell() {
 
     }
 
-    public void addBuffs(){
+    public void changeAp() {
 
     }
 
-    public void deleteBuffs(){
+    public void changeHp() {
 
     }
 
-    public void createUnits(){
+    public void addBuffs() {
+
+    }
+
+    public void deleteBuffs() {
+
+    }
+
+    public void createUnits() {
 
     }
 }
