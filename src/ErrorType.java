b1f728138edd8a23159baae66a7ff4192e9019bf
sepehrs1378 <sepehr.safari8731@gmail.com>
2019-04-18 23:30:35 +0430

    added class ControllerMainMenu

--- src/ErrorType.java
+++ src/ErrorType.java
@@ -1,5 +1,6 @@
 public enum ErrorType {
-    WRONG_COMMAND("wrong command :(");
+    WRONG_COMMAND("wrong command :("),
+    BATTLE_NOT_FINISHED("battle isn't finished yet...");
 
     private String message;
 
