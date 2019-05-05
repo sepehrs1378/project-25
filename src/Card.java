74003409302f0e60d1cb354110f7b2bc061663da
AS2C <ali.saraertoosi@yahoo>
2019-05-05 22:42:13 +0430

    Merge branch 'master' of https://github.com/aps2019project/project-25
    # Conflicts:
    #	src/View.java

--- src/Card.java
+++ src/Card.java
@@ -15,37 +15,7 @@ public class Card {
         this.mana = mana;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public int getMana() {
-        return mana;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setMana(int mana) {
-        this.mana = mana;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    
 
     public Account getOwner() {
         Pattern pattern = Pattern.compile(Constants.ID_PATTERN);
