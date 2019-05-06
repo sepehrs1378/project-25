78f6d8886ba19a1b3c224b363b03ccde422c5f14
AS2C <ali.saraertoosi@yahoo>
2019-05-06 21:04:23 +0430

    adding input_Ali

--- src/HelpType.java
+++ src/HelpType.java
@@ -33,9 +33,9 @@ public enum HelpType {
             "\n\tshow opponent minions" +
             "\n\tshow card info [card id]" +
             "\n\tselectDeckName [card id]" +
-            "\n\tmoveTo to ([row],[column])" +
-            "\n\tattackId [opponent card id]" +
-            "\n\tattackId combo [opponent card id] [my card id] [my card id]..." +
+            "\n\tmove to ([row],[column])" +
+            "\n\tattack [opponent card id]" +
+            "\n\tattack combo [opponent card id] [my card id] [my card id]..." +
             "\n\tuse special power ([row],[column])" +
             "\n\tshow hand" +
             "\n\tinsert [card name] in ([row],[column])" +
