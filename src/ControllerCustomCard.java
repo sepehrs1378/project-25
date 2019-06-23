28444d06f7af744e5c08409ed49f63cdc8f5fd12
parhamsaremi <pa.sa.13792000@outlook.com>
2019-06-23 15:23:25 +0430

    custom card changed

--- src/ControllerCustomCard.java
+++ src/ControllerCustomCard.java
@@ -1,12 +1,22 @@
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerCustomCard implements Initializable {
+    private ObservableList<String> modeList = FXCollections.observableArrayList();
+    @FXML
+    private JFXComboBox<String> heroAttackTypeBox;
+
+    @FXML
+    private JFXComboBox<String> minionAttackTypeBox;
 
     @FXML
     private JFXTextField minionName;
@@ -29,9 +39,69 @@ public class ControllerCustomCard implements Initializable {
     @FXML
     private JFXTextField minionAptxt;
 
+    @FXML
+    private JFXTextField minionRangetxt;
+
+    @FXML
+    private ImageView maxRangeHero;
+
+    @FXML
+    private ImageView maxRangeMinion;
+
+    @FXML
+    private JFXTextField heroRangetxt;
+
+    @FXML
+    void minionComboOnAction(ActionEvent event) {
+
+        try {
+            switch (minionAttackTypeBox.getValue()) {
+                case Constants.MELEE:
+                    maxRangeMinion.setVisible(false);
+                    minionRangetxt.setVisible(false);
+                    break;
+                case Constants.HYBRID:
+                    maxRangeMinion.setVisible(true);
+                    minionRangetxt.setVisible(true);
+                    break;
+                case Constants.RANGED:
+                    maxRangeMinion.setVisible(true);
+                    minionRangetxt.setVisible(true);
+                    break;
+                default:
+                    maxRangeMinion.setVisible(false);
+                    minionRangetxt.setVisible(false);
+                    break;
+            }
+        }catch (Throwable ignore){
+        }
+    }
+
+    @FXML
+    void heroComboOnAction(ActionEvent event) {
+        switch (heroAttackTypeBox.getValue()) {
+            case Constants.MELEE:
+                maxRangeHero.setVisible(false);
+                heroRangetxt.setVisible(false);
+                break;
+            case Constants.HYBRID:
+                maxRangeHero.setVisible(true);
+                heroRangetxt.setVisible(true);
+                break;
+            case Constants.RANGED:
+                maxRangeHero.setVisible(true);
+                heroRangetxt.setVisible(true);
+                break;
+            default:
+                maxRangeHero.setVisible(false);
+                heroRangetxt.setVisible(false);
+                break;
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String style = ("-fx-text-inner-color: #c3c3c3;-fx-prompt-text-fill: #969696");
+        String style = ("-fx-text-inner-color: #c3c3c3;-fx-prompt-text-fill: #969696;");
         minionAptxt.setStyle(style);
         heroHPtxt.setStyle(style);
         heroAPtxt.setStyle(style);
@@ -39,6 +109,19 @@ public class ControllerCustomCard implements Initializable {
         spellName.setStyle(style);
         heroName.setStyle(style);
         minionName.setStyle(style);
+        heroAttackTypeBox.setStyle(style);
+        minionAttackTypeBox.setStyle(style);
+        modeList.add(Constants.MELEE);
+        modeList.add(Constants.HYBRID);
+        modeList.add(Constants.RANGED);
+        minionAttackTypeBox.setItems(modeList);
+        heroAttackTypeBox.setItems(modeList);
+        minionRangetxt.setStyle(style);
+        heroRangetxt.setStyle(style);
+        maxRangeMinion.setVisible(false);
+        minionRangetxt.setVisible(false);
+        heroRangetxt.setVisible(false);
+        maxRangeHero.setVisible(false);
 
     }
 }
