6c2a924a41fdf8a91eae8b7b8e27b9691423e448
AS2C <ali.saraertoosi@yahoo>
2019-07-07 11:57:45 +0430

    leader updatability finished

--- src/ControllerMainMenu.java
+++ src/ControllerMainMenu.java
@@ -3,7 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
